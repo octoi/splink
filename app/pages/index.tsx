44170d584772834db293187a348d2f346e9aea57
Fadhil <fadhilsaheer@gmail.com>
2022-07-27 09:25:13 -0700

    paths

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,16 +1,8 @@
+import { Layout } from '@/components/Layout';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div className='p-5'>
-      <h2 className='text-3xl font-medium'>Hello</h2>
-      <p className='text-xl mt-5'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
-        fugit rem placeat nihil ad ipsum, alias maxime, possimus vitae sint
-        aspernatur. Possimus veritatis sit aperiam, quia at laborum iste unde!
-      </p>
-    </div>
-  );
+  return <Layout></Layout>;
 };
 
 export default Home;
