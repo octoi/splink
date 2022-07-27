16a7e087397f8aa2decd5ce1a209c28ab518e9c8
Fadhil <fadhilsaheer@gmail.com>
2022-07-27 09:24:40 -0700

    colors

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
@@ -6,7 +8,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      app: {
+        'bg-2': '#F6FAFF',
+        'text-1': '#2C4074',
+        'text-2': '#727FA2',
+        'text-3': '#354779',
+        'text-4': '#C0C6D6',
+        'accent-1': '#A974FF',
+        'accent-2': '#F4F8FE',
+        'accent-3': '#58B9FF',
+      },
+    },
   },
   plugins: [],
 };
