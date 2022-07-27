20e7dbaf257e64272817da9a1b68f7fb1e7a47fa
Fadhil <fadhilsaheer@gmail.com>
2022-07-26 20:53:02 -0700

    Tailwind CSS setup

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Home page</div>;
+  return (
+    <div className='p-5'>
+      <h2 className='text-3xl font-medium'>Hello</h2>
+      <p className='text-xl mt-5'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
+        fugit rem placeat nihil ad ipsum, alias maxime, possimus vitae sint
+        aspernatur. Possimus veritatis sit aperiam, quia at laborum iste unde!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
